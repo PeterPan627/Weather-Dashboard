25f99f9177af05c3512806d8b7d41004dd39d472
LeiMing627 <jackperry933jp@gmail.com>
2019-11-23 16:51:32 -0600

    started developing html, wrote pseudocode for js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//  When search button is clicked, read the city name typed by the user
+//  Using saved city name, execute a current condition get request from open weather map api
+//  Parse response to display current conditions
+//  Using saved city name, execute a 5-day forecast get request from open weather map api
+//  Parse response to display forecast for next 5 days underneath current conditions
+//  Save user's search requests and display them underneath search form
+//  When page loads, automatically generate current conditions and 5-day forecast for the last city the user searched for
